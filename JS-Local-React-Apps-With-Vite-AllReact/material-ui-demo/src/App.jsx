03520784f5f56bed990946af50fd6a4bfe464531
HuanCode13 <huan850613@gmail.com>
2023-08-24 17:38:33 +0800

    material ui

---
+++ JS-Local-React-Apps-With-Vite-AllReact/material-ui-demo/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import RatingDemo from './RatingDemo';
+import FormDemo from './FormDemo';
+import Navbar from './Navbar'
+
+
+function App() {
+  return <div>
+    {/* <Button variant="contained" onClick={() => alert("HI!")}>Contained</Button>
+    <Button variant="outlined">Contained</Button>
+    <Button variant="text">Contained</Button>
+    <Button color='success' variant="contained">Contained</Button>
+    <Button
+      color='secondary'
+      size='small'
+      variant="contained"
+      onClick={() => alert("HI!")}
+    >Contained
+    </Button>
+
+    <IconButton color="secondary" aria-label="add an alarm">
+      <AlarmIcon />
+    </IconButton> */}
+
+    {/* <RatingDemo /> */}
+    <Navbar />
+    <FormDemo />
+
+
+  </div>
+}
+
+export default App
